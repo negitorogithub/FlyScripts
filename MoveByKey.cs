8f8bee6dfdf3d71cc8e7a7b9030d7028c44a778b
negitorogithub <unifarproject35@gmail.com>
2018-10-09 20:53:36 +0900

    add ShowTextinterface

--- MoveByKey.cs
+++ MoveByKey.cs
@@ -2,52 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveByKey : MonoBehaviour {
+public class MoveByKey : MonoBehaviour
+{
 
     private Rigidbody rigidBody;
     public float playerSpeed;
     private float defaultSpeed = 1f;
 
     // Use this for initialization
-    void Start () {
-        rigidBody = GetComponent<Rigidbody>();	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //操作
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rigidBody.velocity.magnitude > 1)
+        {
+            return;
+        }
+
+
         Vector3 vector3toSet = new Vector3();
         if (Input.GetKey("up"))
         {
-            vector3toSet.z = defaultSpeed;
+            vector3toSet += rigidBody.transform.forward;
         }
         if (Input.GetKey("down"))
         {
-            vector3toSet.z = -defaultSpeed;
+            vector3toSet -= rigidBody.transform.forward;
         }
         if (Input.GetKey("right"))
         {
-            vector3toSet.x = defaultSpeed;
+            vector3toSet += rigidBody.transform.right;
         }
         if (Input.GetKey("left"))
         {
-            vector3toSet.x = -defaultSpeed;
-        }
-        if (!Input.anyKey)
-        {
-            vector3toSet = Vector3.zero;
+            vector3toSet -= rigidBody.transform.right;
         }
         vector3toSet.Normalize();
         vector3toSet = vector3toSet * playerSpeed;
-        rigidBody.velocity = new Vector3(vector3toSet.x + rigidBody.velocity.x, rigidBody.velocity.y, vector3toSet.z + rigidBody.velocity.z);
-
-
-
-
-
-
-
-
+        rigidBody.velocity += vector3toSet;
 
     }
 }
---
+++ ShowConstatntText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowConstatntText : MonoBehaviour, ShowText {
+
+    public string[] texts;
+
+    private Text textt;
+
+    public void ShowNextText()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
